ca32ed0250bfccd399aca7fd17da2c47ecb697f1
sweat <sweat.eth@gmail.com>
2022-01-01 16:19:36 -0500

    add event listener for wallet button click

--- script.js
+++ script.js
@@ -3,3 +3,9 @@ console.log('Hello World!!')
 function printName() {
   console.log('Henry');
 }
+
+const btn = document.querySelector('#connectButton');
+
+btn.addEventListener('click', () => {
+  
+})
